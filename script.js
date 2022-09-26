8c69a75a1857fd3657059720ad8fedccff7987ae
Felipe Carvalho <fcarvalhoosilva@gmail.com>
2022-09-26 16:36:23 -0300

    criei o header da pagina e editei o menu

---
+++ script.js
@@ -0,0 +1,21 @@
+let profession ;
+if(profession == '' && undefined) {
+console.log('Por que você não trabalha?');}else{
+    if(profession == undefined) {
+        console.log('Vá procurar um emprego')
+    }
+    console.log('Sua Profissão e '+profession);
+switch(profession) {
+    case 'bombeiro':
+        console.log('sua camisa e vermelha')
+        break;
+    case 'policial':
+        console.log('sua camisa e azul')
+        break;
+    case 'politico':
+        console.log('você não usa camisas')
+        break;
+    default:
+        console.log('Você não tem trabalho')
+        break;
+}}
