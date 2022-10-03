e37d8d2323d35811293332ebbfd3222c15637620
Felipe Carvalho <fcarvalhoosilva@gmail.com>
2022-10-03 16:11:19 -0300

    add o js

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-let profession ;
-if(profession == '' && undefined) {
-console.log('Por que você não trabalha?');}else{
-    if(profession == undefined) {
-        console.log('Vá procurar um emprego')
-    }
-    console.log('Sua Profissão e '+profession);
-switch(profession) {
-    case 'bombeiro':
-        console.log('sua camisa e vermelha')
-        break;
-    case 'policial':
-        console.log('sua camisa e azul')
-        break;
-    case 'politico':
-        console.log('você não usa camisas')
-        break;
-    default:
-        console.log('Você não tem trabalho')
-        break;
-}}
-//55
+let u = document.querySelector('.container-menu');
+
+u.style.display = 'none';
+
+function menuu() {
+    u.style.display = 'flex';
+}
+function voltar() {
+    u.style.display = 'none';
+}
+function menuToReturn() {
+    voltar();
+}
