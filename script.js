5f394535e5e171307532d07742ee4c5ed58f92a7
Felipe Carvalho <fcarvalhoosilva@gmail.com>
2022-10-09 14:40:49 -0300

    editei os paragrafos de casa item do menu

--- script.js
+++ script.js
@@ -31,8 +31,9 @@ function nova_tela1() {
     howIam.style.display = 'flex';
     imagem1.style.display = 'flex';
 
-    texto2.innerHTML = '<h1>Ola meu nome é Felipe</h1><br><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis quas quasi, iusto totam minus sunt non sapiente asperiores, commodi itaque beatae cupiditate perspiciatis? Quaerat molestiae nisi in? Deleniti, magni et?</p>';
 
+    document.querySelector('.status1 .titulo').innerHTML = 'Quem sou?'
+    document.querySelector('.first-paragraph').innerHTML = "Ola, me nome e Felipe, moro em Palmas, tocantins. Estudo desenvolvimento front-end, sou apaixonado por tecnologia, meu sonho e tornar-me um programador full-stack completo. Sempre tive o desejo de ser uma pessoa colaborativa, tenho grande desejo de ajudar as pessoas, vi na tecnologia essa oportunidade, a tecnologia e uma poderosa ferramenta que pode beneficiar todas as pessoas do mundo, acredito que ela nunca deixara de ser aprimorada, meu desejo e usar meus conhecimentos para beneficiar a comunidade, empresas e qualquer tipo de instituição com o desenvolvimento de sites, aplicativos ou sistemas."
 }
 
 function nova_tela2() {
@@ -42,7 +43,19 @@ function nova_tela2() {
     howIam.style.display = 'none';
     imagem2.style.display = 'flex';
 
-    texto2.innerHTML = '<h1>hhhhhFelipe</h1><br><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis quas quasi, iusto totam minus sunt non sapiente asperiores, commodi itaque beatae cupiditate perspiciatis? Quaerat molestiae nisi in? Deleniti, magni et?</p>';
+
+    document.querySelector('.status1 .titulo').innerHTML = 'O que faz um programador?'
+    document.querySelector('.status1 .first-paragraph').innerHTML = 'O programador trabalha com o desenvolvimento de programas de computador, celular ou outros equipamentos tecnológicos. Por meio de comandos e códigos eles desenvolvem funções de aplicativos, videogames e computadores, a depender de sua área de atuação. É fundamental, portanto, que o programador entenda linguagens como html, java, javascrip, CSS, SQL, dentre outras. <ul>Veja a seguir os principais tipos de programador: <li>Programador web: desenvolve sites e páginas da internet</li> <li>Programador desktop: desenvolve aplicativos e programas para computadores desktop</li> <li>Programador mobile: trabalha na formação de aplicativos para celulares e tablets</li> <li>Programador games: atua na programação de jogos eletrônicos para tablets, celulares, playstation e computadores</li> <li>Programador executivo: coordena o processo de desenvolvimento de programas </li></ul>';
+}
+function nova_tela3() {
+    telaInformacao.style.display = 'flex';
+    u.style.display = 'none';
+    footer.style.display = 'none';
+    howIam.style.display = 'none';
+    imagem3.style.display = 'flex';
+
+    document.querySelector('.status1 .titulo').innerHTML = 'Quais os desafios da área?'
+
 
 }
 function nova_tela4() {
@@ -52,7 +65,20 @@ function nova_tela4() {
     howIam.style.display = 'none';
     imagem4.style.display = 'flex';
 
-    texto2.innerHTML = '<h1>hhhhhFelipe</h1><br><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis quas quasi, iusto totam minus sunt non sapiente asperiores, commodi itaque beatae cupiditate perspiciatis? Quaerat molestiae nisi in? Deleniti, magni et?</p>';
+    document.querySelector('.status1 .titulo').innerHTML = 'O que faz um programador?'
+
+
+
+}
+function nova_tela5() {
+    telaInformacao.style.display = 'flex';
+    u.style.display = 'none';
+    footer.style.display = 'none';
+    howIam.style.display = 'none';
+    imagem5.style.display = 'flex';
+
+    document.querySelector('.status1 .titulo').innerHTML = 'Meus planos para o futuro?'
+
 
 }
 function nova_tela() {
